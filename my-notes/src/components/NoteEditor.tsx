6755c6ce61c880c62c5dd54c6f104e98569bba5e
Rcheu <rcheung1997@gmail.com>
2026-05-24 21:47:22 -0700

    initialized component

---
+++ my-notes/src/components/NoteEditor.tsx
@@ -0,0 +1,9 @@
+const NoteEditor = () => {
+    return (
+        <div>
+            <h2>Note Editor</h2>
+        </div>
+    )
+}
+
+export default NoteEditor
---
+++ my-notes/src/components/NoteList.tsx
@@ -0,0 +1,9 @@
+const NoteList = () => {
+    return (
+        <div>
+            <h2>Note List</h2>
+        </div>
+    )
+}
+
+export default NoteList
