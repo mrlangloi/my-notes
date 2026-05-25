75e67f25715b242da557e84dad4bb306225918ac
Rcheu <rcheung1997@gmail.com>
2026-05-25 00:52:32 -0700

    added pending state for submit button

--- my-notes/src/components/NoteEditor.tsx
+++ my-notes/src/components/NoteEditor.tsx
@@ -1,7 +1,24 @@
+import { useFormStatus } from "react-dom"
+
 const NoteEditor = () => {
+    const { pending } = useFormStatus()
+
     return (
         <div>
             <h2>Note Editor</h2>
+
+            <form>
+                <label htmlFor="title">Title</label>
+                <input type="text" id="title" name="title" />
+
+                <label htmlFor="body">Body</label>
+                <textarea id="body" name="body" />
+
+                {/* https://react.dev/reference/react-dom/components/form#display-a-pending-state-during-form-submission */}
+                <button type="submit" disabled={pending}>
+                    {pending ? "Saving..." : "Save"}
+                </button>
+            </form>
         </div>
     )
 }
