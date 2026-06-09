2884b9b867c0dcdf28b909e659e4c3c5bbabfbb9
Rcheu <rcheung1997@gmail.com>
2026-06-08 22:48:27 -0700

    added description if no notes are saved

--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -4,6 +4,17 @@ import NoteItem from "./NoteItem"
 const NoteList = () => {
     const { notes } = useNotes()
 
+    if (notes.length === 0) {
+        return (
+            <div className="note-list">
+                <h2>Note List</h2>
+                <small>
+                    No notes yet. Create one using the note editor!
+                </small>
+            </div>
+        )
+    }
+
     return (
         <div className="note-list">
             <h2>Note List</h2>
